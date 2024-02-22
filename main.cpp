b0a26136d52fc20e60a985df504b97d2ae4827c2
danielrmenzel <daniel-menzel-weinheim@hotmail.de>
2024-02-22 07:07:14 +0100

    current state

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     SDLApp sdlApp; // Create the SDLApp instance
+    QObject::connect(&sdlApp, &SDLApp::quitApplication, &app, &QCoreApplication::quit);
 
     MainWindow w(&sdlApp); // Pass the SDLApp instance to the MainWindow constructor
     w.show();
