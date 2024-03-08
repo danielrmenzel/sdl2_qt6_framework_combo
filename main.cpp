db9b2c69ac0325373da8c20dac5df93a0e7a74a6
danielrmenzel <109724990+danielrmenzel@users.noreply.github.com>
2024-03-08 10:20:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
+// main.cpp
 #include <QApplication>
 #include "mainwindow.h"
-#include "SDLApp.h" // Include SDLApp header
+#include "SDLApp.h"
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+    QApplication app(argc, argv);    // CreateQApplication
 
-    SDLApp sdlApp; // Create the SDLApp instance
+    SDLApp sdlApp; // Create SDLApp instance
+
+    
+    /* Connects signals from sender object to receiver object after signal emission
+     * @param &sdlApp - pointer to sender object
+     * @param &SDLApp::quitApplication - trigger signal
+     * @param &app - receiver object (QApplication)
+     * @param &QCoreApplication::quit - terminates event loop
+    */
     QObject::connect(&sdlApp, &SDLApp::quitApplication, &app, &QCoreApplication::quit);
 
-    MainWindow w(&sdlApp); // Pass the SDLApp instance to the MainWindow constructor
+    MainWindow w(&sdlApp); // Pass  SDLApp instance to MainWindow constructor
     w.show();
 
     return app.exec();
 }
+
+
