4c63c8408d6ebc6d4f65dd7226a87da0997d52e5
danielrmenzel <109724990+danielrmenzel@users.noreply.github.com>
2024-04-22 19:07:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,31 @@
-#ifndef SDLAPP_H
-#define SDLAPP_H
+// main.cpp
+#define SDL_MAIN_HANDLED  // Define this before including SDL headers
 
-#include <QObject>
-#include <SDL.h>
-//#include <SDL_ttf.h>
+#include <QApplication>
+#include "mainwindow.h"
+#include "SDLApp.h"
+#include <windows.h>
 
-#include "C:/Users/danie/OneDrive/Desktop/bachelor/local_project_files/SDL2_ttf-2.22.0/include/SDL_ttf.h"
+int main(int argc, char *argv[]) {
+    // Hide the console window
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
 
-//#include <SDL_image.h>
-#include "C:/Users/danie/OneDrive/Desktop/bachelor/local_project_files/SDL2_image-2.8.2/include/SDL_image.h"
-#include <string>
+    QApplication app(argc, argv);    // CreateQApplication
 
-class SDLApp : public QObject {
-    Q_OBJECT
-public:
-    explicit SDLApp(QObject *parent = nullptr);
-    ~SDLApp();
-
-    bool init();
-    bool initResources();
-    bool initTextAndImages();
-    void processEvents();
-    void toggleSDLWindow();
-    SDL_Texture* createTextTexture(const std::string& message, int& textWidth, int& textHeight);
-
-    void submitText(const std::string &text);
-    bool shouldQuit() const;
-    void openOrToggleWindow();
-    void closeWindow();
-    void cleanUpSDLWindow();
-    void cleanUp();
-
-signals:
-    void textEntered(const QString& text);
-    void quitApplication();
-
-private:
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* imageTexture;
-    bool drawLineFlag;
-    bool closeWindowBtnClickedSDL;
-    std::string buttonText;
-
-    // UI dimensions for buttons
-    static constexpr int BUTTON_WIDTH = 200;
-    static constexpr int BUTTON_HEIGHT = 50;
-    int buttonX = 220;
-    int buttonY = 200;
-
-    bool textInputMode;
-    std::string textInputBuffer;
+    SDLApp sdlApp; // Create SDLApp instance
 
 
+    /* Connects signals from sender object to receiver object after signal emission
+     * @param &sdlApp - pointer to sender object
+     * @param &SDLApp::quitApplication - trigger signal
+     * @param &app - receiver object (QApplication)
+     * @param &QCoreApplication::quit - terminates event loop
+    */
+    QObject::connect(&sdlApp, &SDLApp::quitApplication, &app, &QCoreApplication::quit);
 
+    MainWindow w(&sdlApp); // Pass  SDLApp instance to MainWindow constructor
+    w.show();
 
-    TTF_Font* font;
-    SDL_Color textColor;
-
-    std::string submittedText;
-
-    // Rendering methods
-    void render();
-    void renderMessageFromMainwindow(const std::string& message, int x, int y, int fontSize);
-    void renderButton(SDL_Renderer* renderer, const char* text, int x, int y);
-    bool isMouseInsideButton(int mouseX, int mouseY, int buttonX, int buttonY, int buttonWidth, int buttonHeight);
-    void handleButtonClick(SDL_Event& event);
-    void handleTextInput(SDL_Event& event);
-};
-
-#endif // SDLAPP_H
+    return app.exec();
+}
