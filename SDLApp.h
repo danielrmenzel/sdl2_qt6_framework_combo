0da52a9b2e51975cd405ed4174b9e1ad9d20ba18
danielrmenzel <109724990+danielrmenzel@users.noreply.github.com>
2024-04-22 18:23:38 +0200

    Update SDLApp.h

--- SDLApp.h
+++ SDLApp.h
@@ -3,8 +3,12 @@
 
 #include <QObject>
 #include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
+//#include <SDL_ttf.h>
+
+#include "C:/Users/danie/OneDrive/Desktop/bachelor/local_project_files/SDL2_ttf-2.22.0/include/SDL_ttf.h"
+
+//#include <SDL_image.h>
+#include "C:/Users/danie/OneDrive/Desktop/bachelor/local_project_files/SDL2_image-2.8.2/include/SDL_image.h"
 #include <string>
 
 class SDLApp : public QObject {
