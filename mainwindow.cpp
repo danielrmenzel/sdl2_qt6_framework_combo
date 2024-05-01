d432229280ad1b5aba9793754506603a4a780c52
danielrmenzel <109724990+danielrmenzel@users.noreply.github.com>
2024-05-01 15:56:49 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,122 +1,41 @@
-// mainwindow.cpp
+// main.cpp
+#define SDL_MAIN_HANDLED // Define this before including SDL headers
+
+#include <QApplication>
+#include "SDLApp.h"
 #include "mainwindow.h"
-#include <QDebug>
-#include <QFrame>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QLineEdit>
-#include <QMessageBox>
-#include <QPushButton>
-#include <QTimer>
-#include <QVBoxLayout>
-#include "./ui_mainwindow.h"
 
-/*
- * Constructor for MainWindow
- * @param sdlApp - Pointer to SDLApp object to control SDL window
- * @param parent - Pointer to parent widget
- */
-MainWindow::MainWindow(SDLApp *sdlApp, QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow), sdlApp(sdlApp) {
-    ui->setupUi(this); // Setup UI components for this window
+#ifdef Q_OS_WIN
+#include <windows.h>
 
-    // Main vertical layout for the central widget
-    QVBoxLayout *mainLayout = new QVBoxLayout(ui->centralwidget);
+#else
 
-    // Toggle SDL Window Button
-    QPushButton *toggleSDLWindowButton = new QPushButton("Open/Close SDL Window");
-    toggleSDLWindowButton->setStyleSheet("QPushButton { background-color: blue; color: white; }");
-    mainLayout->addWidget(toggleSDLWindowButton);
-    connect(toggleSDLWindowButton, &QPushButton::clicked, this, &MainWindow::onToggleLineButtonClicked);
+#endif
 
-    // Frame to hold the label and display text with an outline
-    QFrame *messageFrame = new QFrame();
-    messageFrame->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
-    messageFrame->setLineWidth(1);
+int main(int argc, char *argv[])
+{
+#ifdef Q_OS_WIN
+    // Hide the console window
 
-    QHBoxLayout *messageLayout = new QHBoxLayout(messageFrame);
-    QLabel *messageLabel = new QLabel("Message from SDL Window:");
-    messageLabel->setStyleSheet("QLabel { color : grey; }");
-    messageLayout->addWidget(messageLabel);
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
 
-    displayTextLabel = new QLabel();
-    displayTextLabel->setMinimumSize(200, 30);
-    messageLayout->addWidget(displayTextLabel);
-    mainLayout->addWidget(messageFrame);
+#endif
 
-    // New section: Horizontal layout for the new label and text input field
-    QHBoxLayout *textInputLayout = new QHBoxLayout();
-    QLabel *textInputLabel = new QLabel("Enter message for SDL2 Window:");
-    textInputField = new QLineEdit();
-    textInputField->setMinimumSize(150, 30);
+    QApplication app(argc, argv); // CreateQApplication
 
-    textInputLayout->addWidget(textInputLabel);
-    textInputLayout->addWidget(textInputField);
-    mainLayout->addLayout(textInputLayout);
+    SDLApp sdlApp; // Create SDLApp instance
 
-    // Submit Button
-    QPushButton *submitButton = new QPushButton("Submit");
-    submitButton->setStyleSheet("QPushButton { background-color: blue; color: white; }");
-    connect(submitButton, &QPushButton::clicked, [this, sdlApp]() {
-        sdlApp->submitText(this->textInputField->text().toStdString());
-    });
+    /* Connects signals from sender object to receiver object after signal emission
+     * @param &sdlApp - pointer to sender object
+     * @param &SDLApp::quitApplication - trigger signal
+     * @param &app - receiver object (QApplication)
+     * @param &QCoreApplication::quit - terminates event loop
+    */
+    QObject::connect(&sdlApp, &SDLApp::quitApplication, &app, &QCoreApplication::quit);
 
-    mainLayout->addWidget(submitButton);
+    MainWindow w(&sdlApp); // Pass  SDLApp instance to MainWindow constructor
+    w.show();
 
-    // Existing SDL Timer for processing events
-    sdlTimer = new QTimer(this);
-    connect(sdlTimer, &QTimer::timeout, sdlApp, &SDLApp::processEvents);
-    sdlTimer->start(16); // Run approximately every 60Hz (16ms)
-
-    // New Timer for checking cursor location
-    QTimer *cursorLocationTimer = new QTimer(this);
-    connect(cursorLocationTimer, &QTimer::timeout, sdlApp, &SDLApp::checkCursorLocation);
-    cursorLocationTimer->start(100); // Check cursor location every 100 milliseconds
-
-    // Connection for SDLApp's textEntered signal
-    bool isConnected = connect(sdlApp, &SDLApp::textEntered, this, &MainWindow::onTextEntered);
-    if (isConnected) {
-        qDebug() << "Successfully connected SDLApp textEntered signal to MainWindow slot.";
-    } else {
-        qDebug() << "Failed to connect SDLApp textEntered signal to MainWindow slot.";
-    }
-
-    // Adjust the window size as needed
-    this->setGeometry(100, 100, 400, 300); // Adjusted for better UI layout visibility
-}
-
-/*
- * Destructor for MainWindow
- */
-MainWindow::~MainWindow() {
-    delete ui;
-}
-
-/*
- * Handles click event of toggle button (open/close)
- */
-void MainWindow::onToggleLineButtonClicked() {
-    sdlApp->openOrToggleWindow();
-}
-
-/*
- * Updates main window's display with submitted text from SDL2 window
- * @param text - Text from SDL2 window
- */
-void MainWindow::onTextEntered(const QString &text) {
-    displayTextLabel->setText(text);
-}
-
-/*
- * Handles window close event, releases SDL resources
- */
-void MainWindow::closeEvent(QCloseEvent *event) {
-    // Stop the SDL timer
-    sdlTimer->stop();
-
-    // Stop the cursor location timer
-    cursorLocationTimer->stop();
-
-    sdlApp->cleanUp();
-    QMainWindow::closeEvent(event);
+    return app.exec();
 }
